69490fb05a89ca3c54d588eaf6bbed1d6d5546be
Gabriel Emiliano Facio <facio.gabrielemiliano@gmail.com>
2024-10-15 23:35:56 -0300

    Distribución de proyecto
    Se implementó la distribución del proyecto en diferentes archivos separando las funciones para mejorar la legilibilidad y la organización del código.

---
+++ funciones.h
@@ -0,0 +1,9 @@
+# pragma once
+
+void tiradaBloqueadores(int v[], int t);
+void tiradaDeDados(int vD[], int tam);
+int dadosNuevos(int vD[5], int vB[2]);
+void mostrarDados(int vD[], int nT);
+int sumarDados(int vD[], int t);
+
+
