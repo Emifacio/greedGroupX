2a5351cde3dae788193d4b798fdd646a6235c2bb
Gabriel Emiliano Facio <facio.gabrielemiliano@gmail.com>
2024-10-24 23:01:52 -0300

    RankingComentado
    Se incorpora funcionalidad de ranking en archivo aparte para ser implementado en un futuro. Se crean los archivos carteles donde se ubicaran los Silly components sin funcionalidad. Dejando en archivo funciones los componentes que ofrecen la lógica en el juego. Se limpia el codigo y se comentan funciones clave para su fácil entendimiento.

--- funciones.cpp
+++ funciones.cpp
@@ -1,102 +1,9 @@
 #include <iostream>
-
 #define FUNCIONES_H
-
 #include <string>
 #include "funciones.h"
+//#include "ranking.h"
 using namespace std;
-
-
-void mostrarCartelGreed() {
-    cout<< endl;
-    cout << "  GGGGG    RRRRR    EEEEE   EEEEE   DDDDD   \n";
-    cout << " G        R    R   E       E       D    D  \n";
-    cout << " G  GGG   RRRRR    EEEE    EEEE    D    D  \n";
-    cout << " G    G   R  R     E       E       D    D  \n";
-    cout << "  GGGGG   R   R    EEEEE   EEEEE   DDDDD   \n";
-    cout << endl;
-}
-
-
-void pedirNombre(string &jugador1, string &jugador2){
-cout   << "Ingresar nombre del jugador nro1 :  "<< endl;
-cin >> jugador1;
-cout   << "Ingresar nombre del jugador nro2 :  "<< endl;
-cin >> jugador2;
-}
-
-
-int mostrarMenu() {
-
-    cout << "Bienvenido al menu principal" << endl;
-    cout << endl;
-    int selection = 0;
-        cout << " Por favor elija una de las siguientes opciones - \n";
-        cout << " 1. Modo un jugador.\n";
-        cout << " 2. Modo dos jugadores.\n";
-        cout << " 3. Estadisticas.\n";
-        cout << " 4. Creditos.\n";
-        cout << " 5. Salir  .\n";
-        cout << "\t";
-        cin >> selection;
-
-        return selection;
-}
-
-
-
-void procesarSeleccion(int selection) {
-    switch (selection) {
-        case 1:
-            cout << "Has seleccionado la opción 1 (Modo un jugador)" << endl;
-            cout << endl;
-            cout << "==================================="<< endl;
-            cout << endl;
-            //juegaModo1Jugador();
-            break;
-        case 2:
-            cout << "Has seleccionado la opción 2 (Modo dos jugadores)" << endl;
-            cout << endl;
-            cout << "==================================="<< endl;
-            cout << endl;
-            //JuegaModo2Jugadores();
-            break;
-        case 3:
-            cout << "Has seleccionado la opción 3 (Estadísticas)" << endl;
-            cout << endl;
-            cout << "==================================="<< endl;
-            cout << endl;
-            break;
-            // mostrarEstadisticas();
-        case 4:
-            cout << "Has seleccionado la opción 4 (Créditos)" << endl;
-            cout << endl;
-            cout << "==================================="<< endl;
-            cout << endl;
-            break;
-            // mostrarCreditos();
-        case 5:
-            cout << "Gracias por jugar GreedX, espero verte pronto nuevamente!" << endl;
-           // mostrarRecord();
-            break;
-        default:
-            cout << "Opción no válida" << endl;
-            break;
-    }
-}
-
-void JuegaModo2Jugadores(string &jugador1,
-    string &jugador2,
-    int &ronda,
-    int aP[3],
-    int aP2[3],
-    int &aPt,
-    int &aPt2,
-    bool &duplicar,
-    char &pregunta){
-
-    }
-
 
 
 // Esta función genera los "dados bloqueadores" que son 2 dados aleatorios.
@@ -108,10 +15,15 @@ void tiradaBloqueadores(int v[],int t)
     cout<<"==================================="<<endl;
     for(x=0; x<2; x++)
     {
-        dado=(rand()%6+1);
+        dado=tirarDado();
         v[x]=dado;
 
     }
+}
+
+
+int tirarDado(){
+    return rand() % 6 + 1;
 }
 
 
@@ -125,9 +37,11 @@ void tiradaDeDados(int vD[], int tam)
     cout << "===================================" << endl;
     for (x = 0; x < tam; x++) // Itera cinco veces para generar cinco dados
     {
-        vD[x] = (rand() % 6 + 1); // Genera un número aleatorio entre 1 y 6 para cada dado
+        vD[x] = tirarDado(); // Genera un número aleatorio entre 1 y 6 para cada dado
         }
 }
+
+
 
 bool dadosIguales(int vD[], int t)
 {
@@ -152,38 +66,33 @@ bool dadosIguales(int vD[], int t)
 
 }
 
-//cout<<"dados con descuentos"<<endl;
+// Esta función filtra los dados que coinciden con los bloqueadores y devuelve cuántos dados quedan disponibles.
 int dadosNuevos(int vD[], int t, int vB[2])
 {
-    t;
-    vD[t]= {};
-    int x,cD=0;
-    for(x=0; x<t; x++)
-    {
-        if(vD[x]==vB[0] || vD[x]==vB[1])
-        {
-            vD[x]=0;
-
+    t;  // `t` es el número de dados (5), `cD` es el contador de dados que quedan
+    vD[t] = {};
+    int x, cD = 0;
+    for(x = 0; x<t; x++) { // Itera sobre los 5 dados
+            // Si el dado actual coincide con alguno de los dados bloqueadores, lo elimina (lo convierte en 0)
+        if(vD[x] == vB[0] || vD[x] == vB[1]) {
+            vD[x] = 0;
+        }
+    } for(x = 0; x<t; x++) { // Esta segunda iteración filtra los dados que no han sido bloqueados (es decir, los que no son 0)
+        if(vD[x] != 0) { // Si el dado no es 0, lo mueve al principio del vector
+            vD[cD] = vD[x]; // Reorganiza los dados para eliminar espacios vacíos (0)
+            cD++; // Incrementa el contador de dados disponibles
         }
     }
-    for(x=0; x<t; x++)
-    {
-        if(vD[x]!=0)
-        {
-            vD[cD]=vD[x];
-            cD++;
-        }
-    }
-   return cD;
+   return cD; // Devuelve cuántos dados no fueron bloqueados
 }
 
-
+// Esta función muestra los dados actuales después de haber sido filtrados.
 void mostrarDados(int vD[], int tam)
 {
     int x;
-    for(x=0; x<tam; x++)
+    for(x = 0; x<tam; x++)// Muestra los dados disponibles después de filtrar los bloqueados
     {
-        cout<< "Dado : " << vD[x] << endl;
+        cout << "Dado : " << vD[x] << endl;
     }
 }
 
@@ -198,45 +107,6 @@ int sumarDados(int vD[], int t)
 }
 
 
-/*
-// Esta función filtra los dados que coinciden con los bloqueadores y devuelve cuántos dados quedan disponibles.
-int dadosNuevos(int vD[5], int vB[2])
-{
-    int x, t = 5, cD = 0; // `t` es el número de dados (5), `cD` es el contador de dados que quedan
-    for (x = 0; x < t; x++) // Itera sobre los 5 dados
-    {
-        // Si el dado actual coincide con alguno de los dados bloqueadores, lo elimina (lo convierte en 0)
-        if (vD[x] == vB[0] || vD[x] == vB[1])
-        {
-            vD[x] = 0;
-        }
-    }
-     // Esta segunda iteración filtra los dados que no han sido bloqueados (es decir, los que no son 0)
-    for (x = 0; x < t; x++)
-    {
-        if (vD[x] != 0) // Si el dado no es 0, lo mueve al principio del vector
-        {
-            vD[cD] = vD[x]; // Reorganiza los dados para eliminar espacios vacíos (0)
-            cD++; // Incrementa el contador de dados disponibles
-        }
-    }
-    return cD; // Devuelve cuántos dados no fueron bloqueados
-}
 
-
-
-// Esta función muestra los dados actuales después de haber sido filtrados.
-void mostrarDados(int vD[], int nT)
-{
-    int x;
-    for (x = 0; x < nT; x++) // Muestra los dados disponibles después de filtrar los bloqueados
-    {
-        cout << "Dado " << vD[x] << " - ";
-    }
-}
-*/
-
-
-// Esta función suma los valores de los dados que no han sido bloqueados. Es decir, suma CADA TIRADA.
 
 
