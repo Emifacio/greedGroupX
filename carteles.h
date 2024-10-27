d1cc920451b3fcfa898bd7857164af1c04f7b594
RaulLuppini <raulluppini@gmail.com>
2024-10-27 19:55:21 -0300

    prueba
    saque los usins namespace std de los h

--- carteles.h
+++ carteles.h
@@ -1,14 +1,14 @@
 #pragma once
-using namespace std;
+
 
 /**Etiquetas de funciones de mostrares para
 mostrar durante el programa.*/
 
 void mostrarmostrarGreed() ;
-void pedirNombre(string &jugador1, string &jugador2);
+void pedirNombre(std::string &jugador1, std::string &jugador2);
 int mostrarMenu();
 void procesarSeleccion(int selection);
-void mostrarPrimerTurno(string jugador1);
+void mostrarPrimerTurno(std::string jugador1);
 void mostrarRonda(int ronda);
 void mostrarTituloDadosQSuman();
 void mostrarNroTirada(int cDtiradas);
@@ -18,10 +18,10 @@ char preguntaContinuarTirando(char pregunta);
 void mostrarNroTirada(int cDtiradas);
 void mostrarPuntajeTirada(int puntaje);
 void mostrarPuntajeRonda(int ronda, int aP[]);
-void mostrarMsjCambioJugador(string jugador);
+void mostrarMsjCambioJugador(std::string jugador);
 void mostrarMsjSumaCero();
-void mostrarMsjFinRondas(string jugador, int ronda, int aP[]);
-void mostrarTotalPtsAmbosJugadores(string jugador1, string jugador2, int acumuladoPuntajeTotalJugador1, int acumuladoPuntajeTotalJugador2);
-void mostrarGanador(string jugador);
+void mostrarMsjFinRondas(std::string jugador, int ronda, int aP[]);
+void mostrarTotalPtsAmbosJugadores(std::string jugador1, std::string jugador2, int acumuladoPuntajeTotalJugador1, int acumuladoPuntajeTotalJugador2);
+void mostrarGanador(std::string jugador);
 void mostrarMsjFinalJuego();
 void mostrarBarraDivisora();
--- funciones.h
+++ funciones.h
@@ -1,10 +1,10 @@
 # pragma once
-using namespace std;
 
 
 
-void pedirNombre(string jugador1, string jugador2);
-void acumularPuntaje(int puntajeJ1, int puntajeJ2, vector puntajeRonda[]);
+
+void pedirNombre(std::string jugador1,std:: string jugador2);
+void acumularPuntaje(int puntajeJ1, int puntajeJ2, int puntajeRonda[]);
 int mostrarMenu();
 int procesarSeleccion(int seleccion);
 void tiradaBloqueadores(int v[], int t);
@@ -16,10 +16,10 @@ void mostrarDados(int vD[], int nT);
 
 
 void mostrarCartelGreed();
-void pedirNombre(string &jugador1, string &jugador2);
+void pedirNombre(std::string &jugador1, std::string &jugador2);
 //void acumularPuntaje(int puntajeJ1, int puntajeJ2, vector puntajeRonda[]);
 int mostrarMenu();
-void procesarSeleccion(int seleccion);
+//void procesarSeleccion(int seleccion);
 void tiradaBloqueadores(int v[], int t);
 int tirarDado();
 void tiradaDeDados(int vD[], int tam);
